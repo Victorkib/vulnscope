ad1b566e343a1ce8de0aa0fceae7d0896dd53b6b
victorkib <victor3720kibiwott@gmail.com>
2025-09-11 11:46:36 +0300

    changes

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,11 +1,10 @@
 import { MongoClient, type Db } from 'mongodb';
 
-// if (!process.env.MONGODB_URI!) {
-//   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
-// }
+if (!process.env.MONGODB_URI!) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
 
-const uri = 'mongodb+srv://qinalexander56:Safaricom360@cluster0.fr14aej.mongodb.net/VulnScope?retryWrites=true&w=majority';
-const NODE_ENV = 'development';
+const uri = process.env.MONGODB_URI!;
 const options = {
   maxPoolSize: 10,
   serverSelectionTimeoutMS: 5000,
@@ -15,7 +14,7 @@ const options = {
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-if (NODE_ENV! === 'development') {
+if (process.env.NODE_ENV! === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
   const globalWithMongo = global as typeof globalThis & {
